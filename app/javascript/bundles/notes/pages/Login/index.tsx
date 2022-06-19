7c87d788b4219b4d5764c194f08009aa88e6bb2e
Arly Xiao <kingla_pei@163.com>
2022-06-19 20:23:16 +0800

    Add style demo

--- app/javascript/bundles/notes/pages/Login/index.tsx
+++ app/javascript/bundles/notes/pages/Login/index.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import { AuthTemplate } from '../../templates/Auth'
+import styles from './styles.module.css';
+
 
 export const Login = (props) => {
   return (
     <AuthTemplate>
+      <p className={styles.demo}>
       Login: {props.title}
+
+      </p>
     </AuthTemplate>
   )
 }
